a9f6c5b8c5ae5252ad72bc45c71017a264f5361d
munaim <munaim@pmc.edu.pk>
2026-01-26 17:48:31 +0500

    kk

--- backend/accredcheck/settings.py
+++ backend/accredcheck/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
   "corsheaders.middleware.CorsMiddleware",
   "django.middleware.security.SecurityMiddleware",
+  "whitenoise.middleware.WhiteNoiseMiddleware",
   "django.contrib.sessions.middleware.SessionMiddleware",
   "django.middleware.common.CommonMiddleware",
   "django.middleware.csrf.CsrfViewMiddleware",
@@ -62,12 +63,22 @@ USE_I18N=True
 USE_TZ=True
 
 STATIC_URL="static/"
+STATIC_ROOT=BASE_DIR/"staticfiles"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 DEFAULT_AUTO_FIELD="django.db.models.BigAutoField"
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.getenv("MEDIA_ROOT", str(BASE_DIR/"media"))
 
 CORS_ALLOWED_ORIGINS=[o.strip() for o in os.getenv("CORS_ALLOWED_ORIGINS","").split(",") if o.strip()]
+CSRF_TRUSTED_ORIGINS=[o.strip().replace("http://","https://") for o in os.getenv("CORS_ALLOWED_ORIGINS","").split(",") if o.strip()]
 
 REST_FRAMEWORK = {
   "DEFAULT_AUTHENTICATION_CLASSES":[
